07080a5d57a8064c97198aead5ddaf9f982bc86d
mar <mar.nospam@anomy.net>
2010-10-30 21:23:48 +0000

    simplified and optimized the script quite a bit - without changing it's behaviour one bit.

--- jquery.lettering.js
+++ jquery.lettering.js
@@ -8,50 +8,46 @@
 *
 * Thanks to Paul Irish - http://paulirish.com - for the feedback.
 *
-* Date: Mon Sep 20 17:14:00 2010 -0600
+* Optimized by Már Örlygsson - http://mar.anomy.net/
 */
 (function($){
-	function injector(t, splitter, klass, after) {
-		var a = t.text().split(splitter), inject = '';
-		if (a.length) {
-			$(a).each(function(i, item) {
-				inject += '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
-			});	
-			t.empty().append(inject);
-		}
+	var injector = function (c, splitter, klass, after) {
+		c.each(function(){
+			var t = $(this),
+			    a = t.text().split(splitter),
+			    inject = '';
+			if (a.length) {
+				$(a).each(function(i, item) {
+					inject += '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
+				});	
+				t.html(inject);
+			}
+		});
 	}
 	var methods = {
-		init : function() {
-			return this.each(function() {
-				injector($(this), '', 'char', '');
-			});
+		letters : function ( t ) {
+			injector( t, '', 'char', '' );
 		},
-		words : function() {
-			return this.each(function() {
-				injector($(this), /\s+/, 'word', ' ');
-			});
+		words : function( t ) {
+			injector( t, /\s+/, 'word', ' ' );
 		},
-		lines : function() {
-			return this.each(function() {
-				var r = "eefec303079ad17405c889e092e105b0";
-				// Because it's hard to split a <br/> tag consistently across browsers,
-				// (*ahem* IE *ahem*), we replaces all <br/> instances with an md5 hash 
-				// (of the word "split").  If you're trying to use this plugin on that 
-				// md5 hash string, it will fail because you're being ridiculous.
-				injector($(this).children("br").replaceWith(r).end(), r, 'line', '');
-			});
-
+		lines : function( t ) {
+			var r = 'eefec303079ad17405c889e092e105b0';
+			// Because it's hard to split a <br/> tag consistently across browsers,
+			// (*ahem* IE *ahem*), we replaces all <br/> instances with an md5 hash 
+			// (of the word "split").  If you're trying to use this plugin on that 
+			// md5 hash string, it will fail because you're being ridiculous.
+			injector( t.find('br').replaceWith(r).end(), r, 'line', '' );
 		}
 	};
 
 	$.fn.lettering = function( method ) {
-		// Method calling logic
-		if ( method && methods[method] ) {
-			return methods[ method ].apply( this, [].slice.call( arguments, 1 ));
-		} else if ( method === 'letters' || ! method ) {
-			return methods.init.apply( this, [].slice.call( arguments, 0 ) ); // always pass an array
+		method = method || 'letters';
+		if ( methods[method] ) {
+			methods[ method ]( this );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.lettering' );
 		}
-		$.error( 'Method ' +  method + ' does not exist on jQuery.lettering' );
 		return this;
 	};
 
