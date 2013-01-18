1452e21a40de24863d645007a083e80da3242b92
RamonPage <ramonpage@gmail.com>
2013-01-17 23:25:09 -0200

    Adding support for optional function arguments.

--- jquery.lettering.js
+++ jquery.lettering.js
@@ -1,9 +1,9 @@
 /*global jQuery */
-/*!	
+/*!
 * Lettering.JS 0.6.1
 *
 * Copyright 2010, Dave Rupert http://daverupert.com
-* Released under the WTFPL license 
+* Released under the WTFPL license
 * http://sam.zoy.org/wtfpl/
 *
 * Thanks to Paul Irish - http://paulirish.com - for the feedback.
@@ -11,21 +11,22 @@
 * Date: Mon Sep 20 17:14:00 2010 -0600
 */
 (function($){
-	function injector(t, splitter, klass, after) {
-		var a = t.text().split(splitter), inject = '';
+	function injector(options) {
+    var opt = options || {};
+		var a = opt.t.text().split(opt.splitter), inject = '';
 		if (a.length) {
 			$(a).each(function(i, item) {
-				inject += '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
-			});	
-			t.empty().append(inject);
+				inject += '<span class="'+opt.klass+(i+1)+'">'+item+'</span>'+opt.after;
+			});
+			opt.t.empty().append(inject);
 		}
 	}
-	
+
 	var methods = {
 		init : function() {
 
 			return this.each(function() {
-				injector($(this), '', 'char', '');
+				injector({t: $(this), splitter: '', klass: 'char', after: ''});
 			});
 
 		},
@@ -33,20 +34,20 @@
 		words : function() {
 
 			return this.each(function() {
-				injector($(this), ' ', 'word', ' ');
+				injector({t: $(this), splitter: ' ', klass: 'word', after: ' '});
 			});
 
 		},
-		
+
 		lines : function() {
 
 			return this.each(function() {
 				var r = "eefec303079ad17405c889e092e105b0";
 				// Because it's hard to split a <br/> tag consistently across browsers,
-				// (*ahem* IE *ahem*), we replaces all <br/> instances with an md5 hash 
-				// (of the word "split").  If you're trying to use this plugin on that 
+				// (*ahem* IE *ahem*), we replaces all <br/> instances with an md5 hash
+				// (of the word "split").  If you're trying to use this plugin on that
 				// md5 hash string, it will fail because you're being ridiculous.
-				injector($(this).children("br").replaceWith(r).end(), r, 'line', '');
+				injector({t: $(this).children("br").replaceWith(r).end(), splitter: r, klass: 'line', after: ''});
 			});
 
 		}
